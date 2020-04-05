1d3440861866576065fcc830ffcad5ec27969d5f
humpangle <humpangle@gmail.com>
2020-04-05 19:01:30 +0200

    updated

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // simple node web server that displays hello world
 // optimized for Docker image
 
-const express = require('express');
+const express = require("express");
 // this example uses express web framework so we know what longer build times
 // do and how Dockerfile layer ordering matters. If you mess up Dockerfile ordering
 // you'll see long build times on every code change + build. If done correctly,
 // code changes should be only a few seconds to build locally due to build cache.
 
-const morgan = require('morgan');
+const morgan = require("morgan");
 // morgan provides easy logging for express, and by default it logs to stdout
 // which is a best practice in Docker. Friends don't let friends code their apps to
 // do app logging to files in containers.
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 // this example includes a connection to MongoDB
 
 const {
@@ -20,7 +20,7 @@ const {
   MONGO_PASSWORD,
   MONGO_HOSTNAME,
   MONGO_PORT,
-  MONGO_DATABASE_NAME
+  MONGO_DATABASE_NAME,
 } = process.env;
 
 // Connection URL
@@ -32,7 +32,7 @@ const client = new MongoClient(url);
 let db;
 // Use connect method to connect to the Server
 setTimeout(() => {
-  client.connect(function(err) {
+  client.connect(function (err) {
     if (err) {
       return console.error(err);
     }
@@ -44,33 +44,35 @@ setTimeout(() => {
 // Api
 const app = express();
 
-app.use(morgan('common'));
+app.use(morgan("common"));
 
-app.get('/', function (req, res) {
-  res.send('Hello Docker World\n');
+app.get("/", function (req, res) {
+  res.send("Hello Docker World\n.Lovely people");
 });
 
-app.get('/healthz', function (req, res) {
-	// do app logic here to determine if app is truly healthy
-	// you should return 200 if healthy, and anything else will fail
-	// if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
-  res.send('I am happy and healthy and then......\nBut what does that mean');
+app.get("/healthz", function (req, res) {
+  // do app logic here to determine if app is truly healthy
+  // you should return 200 if healthy, and anything else will fail
+  // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
+  res.send("I am happy and healthy and then......\nBut what does that mean");
 });
 
-app.get('/documents', function (req, res, next) {
+app.get("/documents", function (req, res, next) {
   // might have not been connected just yet
   if (db) {
-    db.collection('documents').find({}).toArray(function(err, docs) {
-      if (err) {
-        console.error(err);
-        next(new Error('Error while talking to database'));
-      } else {
-        res.json(docs);
-      }
-    });
+    db.collection("documents")
+      .find({})
+      .toArray(function (err, docs) {
+        if (err) {
+          console.error(err);
+          next(new Error("Error while talking to database"));
+        } else {
+          res.json(docs);
+        }
+      });
   } else {
-    next(new Error('Waiting for connection to database'));
+    next(new Error("Waiting for connection to database"));
   }
-})
+});
 
 module.exports = app;
